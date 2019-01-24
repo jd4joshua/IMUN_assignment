d0deb2ef42811befe5f85be6a787fb93a9b581ed
Joshua Dani M <36666984+jd4joshua@users.noreply.github.com>
2019-01-24 17:36:27 +0530

    Add files via upload

---
+++ imun/index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>International MUN | Home</title>
+	<!-- Meta tags -->
+	<meta charset="utf-8">	
+	<meta name="description" content="International Model United Nations (IMUN) brings youth together from around the world to learn and share ideas from a diverse set of experiences and backgrounds where the Executive board, International Press and International Delegates consolidate to learn about diplomacy, international relations, and the United Nations.">
+	<meta name="author" content="IMUN">
+	<meta name = "application-name" content="International Model United Nations">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<!-- Favicon -->
+	<link rel="icon" href="img/logo.png" type="image/png">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/custom.css">
+	<!-- Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,700,900,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,300,600,700,900' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top py-0">
+		<div class="container">
+			<a class="navbar-left navbar-brand" href="index.php">
+			<img src = "img/logo2.png" alt = "imun_logo" id = "imun_logo">
+			<p>International <small><br>Model United Nations</small></p>				
+			</a>
+		</div>
+		<div class="navbar-center navbar-brand">
+			<ul class="navbar-nav navbar-light" >
+					<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+					<li class="nav-item"><a class="nav-link" href="Aboutus.php">About Us</a></li>
+					<li class = "nav-item dropdown">
+						<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Conferences</a>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="Conference.php">IMUN 2018</a>
+							<a class="dropdown-item" href = "http://www.internationalmun.org/IMUN_Hanoi_2019.php">IMUN Hanoi 2019</a>
+						</div>
+					</li>
+					<li class="nav-item"><a class="nav-link" href = "Ourteam.php">Our Team</a></li>
+					<li class="nav-item"><a class="nav-link" href="OnCampus.php">On-Campus</a></li>
+					<li class="nav-item"><a class="nav-link" href="Jointeam.php">Join Us</a></li>
+					<li class="nav-item"><a class="nav-link" href="Sponsorus.php">Sponsor Us</a></li>
+					<li class="nav-item"><a class="nav-link" href="http://blog.internationalmun.org/" target="_blank">Blog</a></li>
+					<li class="nav-item"><a class="nav-link" href="Contactus.php">Contact Us</a></li>
+			</ul>
+		</div>
+	</nav>
+	<div class="wrapper" id="wrapper">
+	<!--header section-->
+		<div class="jumbotron jumbotron-fluid" id="banner">       
+			<div class="parallax text-center" style="background-image: url(img/hanoi.jpg);opacity: 0.8;">
+					<div class="imun text-center" style="height:600px;padding-top:160px;">	
+						<p></p>
+						<h2 class="glow">International</h2>
+						<h2 class="glow">Model United Nations</h2>
+						<h3 class="glow">2019</h3>
+						<h2>
+							<a href="#" class="register">Register Now!</a>
+						</h2>
+					</div>					
+			</div>
+		</div>
+	</div>
+	<!--about us section-->
+	<section class="aboutus" id="aboutus">
+	<div class="parallax text-center" style="background: radial-gradient(lightblue,white);opacity: 0.9;">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading text-center">
+						<h2>About Us</h2>
+						<h3><b>International Model United Nations (IMUN)</b> brings people together from around the world to learn and share ideas from a diverse 
+						set of experiences and backgrounds where the Executive board, International Press and International Delegates consolidate to learn about 
+						diplomacy, international relations, and the United Nations.</h3>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card card-inverse text-center">
+						<img class="card-img-top" src="img/card1.jpg" alt="Card image cap">
+							<div class="card-block">
+								<h4 class="card-title">Methodology</h4>
+								At IMUN, delegates gain insight into the United Nations and the dynamics of international relations by assuming the roles of UN 
+								representatives and members of other international bodies and national cabinets.
+								<p></p><center><a href="#" class="btn btn-primary">Learn More</a><center>
+							</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card card-inverse card-primary text-center">
+						<img class="card-img-top" src="img/card2.jpg" alt="Card image cap">
+							<div class="card-block">
+								<h4 class="card-title">Our Mission</h4>
+								True to the spirit of the United Nations, IMUN strives to foster a constructive forum for open dialogue on complex global issues, 
+								including international peace and security and economic and social progress. IMUN stresses the in-depth examination and resolution of 
+								pressing issues by emphasizing the process over results. 
+								<p></p><center><a href="#" class="btn btn-primary">Learn More</a></center>
+							</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card card-inverse card-success text-center">
+						<img class="card-img-top" src="img/card3.jpg" alt="Card image cap">
+							<div class="card-block">
+								<h4 class="card-title">Objectives</h4>
+								•	Develop deeper knowledge on current global issues and improve critical thinking.<p></p>•	Strengthen their diplomacy skills 
+								and develop ideas to solve current international problems.
+								<p></p><center><a href="#" class="btn btn-primary">Learn More</a></center>
+							</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</section>
+
+	<!--Sponsor section-->
+	<section class="sponsor" id="sponsor">
+	<div class="parallax text-center" style="background: radial-gradient(lightblue,white);opacity: 0.9;">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading text-center">
+						<h2><b>Sponsor of <span style = "color:rgb(21,71,122);">IMUN 2018</span></h2>
+						<div class = "row" id = "row-content">
+							<div class = "col-md-5"></div>
+							<div class = "col-md-2">
+								<a href = "https://dfat.gov.au" target = "_blank"><img src="img/ae_logo.jpg"></a>				
+							</div>
+						</div>
+						<h3>The department provides foreign, trade and development policy advice to the government. We work with other government agencies to 
+						ensure that Australia’s pursuit of its global, regional and bilateral interests is coordinated effectively.</h3>
+					</div>
+				</div>
+			</div><br/><br/>
+			<h2>Partners Organisations</h2>
+			<div id="demo" class="carousel slide carousel-fade" data-ride="carousel" data-interval="2500">
+				<div class="carousel-inner">
+				<div class="carousel-item active"><a href = "http://thailand.iom.int" target = "_blank"><img src="img/c1.jpg" class="logos"></a><p>International Organization for Migration (IOM)</p></div>
+				<div class="carousel-item"><a href = "https://zumun.ch/" target = "_blank"><img src="img/c2.png" class="logos"></a><p>Zurich Model United Nations (ZuMUN)</p></div>
+				<div class="carousel-item"><a href = "http://www.internationalcareersfestival.org/" target = "_blank"><img src="img/c3.jpg" class="logos"></a><p>International Careers Festival</p></div>
+				<div class="carousel-item"><a href = "http://teimun.org/" target = "_blank"><img src="img/c4.jpg" class="logos"></a><p>The European International Model United Nations</p></div>
+				<div class="carousel-item"><a href = "http://bmun.benilde.edu.ph/" target = "_blank"><img src="img/c5.jpg" class="logos"></a><p>Benilde Model United Nations</p></div>
+				<div class="carousel-item"><a href = "https://www.facebook.com/IR.StudentUpdates/?ref=br_rs" target = "_blank"><img src="img/c6.jpg" class="logos"></a><p>IR Student Updates</p></div>
+				<div class="carousel-item"><a href = "https://www.amunc2019.com/" target = "_blank"><img src="img/c7.png" class="logos"></a><p>Asia-Pacific Model United Nations</p></div>
+			</div>
+				<a class="carousel-control-prev" href="#demo" data-slide="prev">
+					<i class="fa fa-arrow-circle-left fa-2x your-custom-class" style="background-color:black;" aria-hidden="true"></i>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#demo" data-slide="next">
+					<i class="fa fa-arrow-circle-right fa-2x your-custom-class" style="background-color:black;" aria-hidden="true"></i>
+					<span class="sr-only">Previous</span>
+				</a>
+			</div>
+		</div>
+	</div>
+	</section>
+	<!--testimonials section-->
+	<section class="testimonials">
+	<div class="parallax text-center" style="background: radial-gradient(lightblue,white);opacity: 0.9;"><br/><br/>
+		<div class="container">
+			<div class="row">
+			<div class="col-sm-3"></div>
+				<div class="col-sm-6">
+					<h2><span style = "color:rgb(21,71,122);">Testimonials</span></h2>
+						<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+							<div class="carousel-inner">
+								<div class="carousel-item active">
+									<div class="row" style="padding: 20px">
+										<button style="border: none;"><i class="fa fa-quote-left testimonial_fa" aria-hidden="true"></i></button>
+										<p class="testimonial_para">I’m so happy to have met all the members of IMUN 2018. Who would have known that from this conference I met my new family!</p><br>
+										<img src="img/t1.jpg" class="img-responsive" >
+										<div class="col-sm-10">
+											<h4><strong>Nadya Andra Martinez</strong></h4>
+											<p class="testimonial_subtitle"><span>Indonesia</span><br>
+											</p>
+										</div>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<div class="row" style="padding: 20px">
+										<button style="border: none;"><i class="fa fa-quote-left testimonial_fa" aria-hidden="true"></i></button>
+										<p class="testimonial_para">Another blessing happened to me this year. I hope from here I can reach greater heights in the future!</p><br>
+										<img src="img/t1.jpg" class="img-responsive" >
+										<div class="col-sm-10">
+											<h4><strong>Adellyn Dwitara</strong></h4>
+											<p class="testimonial_subtitle"><span>Indonesia</span><br>
+											</p>
+										</div>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<div class="row" style="padding: 20px">
+										<button style="border: none;"><i class="fa fa-quote-left testimonial_fa" aria-hidden="true"></i></button>
+										<p class="testimonial_para">This is my first Model United Nations conference where I have gained a lot of new experience and have met people who are smart, kind and hardworking. Most importantly, it made me want to improve myself more!</p><br>
+										<img src="img/t1.jpg" class="img-responsive" >
+										<div class="col-sm-10">
+											<h4><strong>Sand Siwapron</strong></h4>
+											<p class="testimonial_subtitle"><span>Thailand</span><br>
+											</p>
+										</div>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<div class="row" style="padding: 20px">
+										<button style="border: none;"><i class="fa fa-quote-left testimonial_fa" aria-hidden="true"></i></button>
+										<p class="testimonial_para">It’s my first MUN experience! Never too late to take the first step!</p><br>
+										<img src="img/t2.jpg" class="img-responsive" >
+										<div class="col-sm-10">
+											<h4><strong>Vineeth Dasari</strong></h4>
+											<p class="testimonial_subtitle"><span>India</span><br>
+											</p>
+										</div>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<div class="row" style="padding: 20px">
+										<button style="border: none;"><i class="fa fa-quote-left testimonial_fa" aria-hidden="true"></i></button>
+										<p class="testimonial_para">The best experience I’ve ever had. Thank you IMUN 2018 for giving me the chance to join the event and winning the ‘Best Position Paper’ award!</p><br>
+										<img src="img/t1.jpg" class="img-responsive" >
+										<div class="col-sm-10">
+											<h4><strong>Popy Puspitasari</strong></h4>
+											<p class="testimonial_subtitle"><span>Indonesia</span><br>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="controls testimonial_control pull-right">
+						<a class="left fa fa-chevron-left btn btn-default testimonial_btn" href="#carousel-example-generic" data-slide="prev"></a>
+						<a class="right fa fa-chevron-right btn btn-default testimonial_btn" href="#carousel-example-generic" data-slide="next"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--footer section-->
+	<section class="footer" id="footer">
+		<div class="container">
+			<div class = "col-md-5" style="text-align:left;">
+				<h4><b>Contact Us</b></h4>
+				<p><b>Email:</b> info@internationalmun.org </p>
+				<p><b>Address:</b> E-73 Sector 50, Gautam Budh Nagar, Noida, India - 201 304 </p>
+				<p><b>Phone:</b> (+91) 999 741 5973</p>
+			</div>
+			<div class="row">
+				<div class="col-lg-12"><hr/>
+					<ul>  
+						<li><a href="https://www.facebook.com/internationalmun2018/" target = "_blank"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="https://twitter.com/imun_18" target = "_blank"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="https://www.linkedin.com/company/international-mun/" target = "_blank"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="https://www.instagram.com/international_mun/" target = "_blank"><i class="fa fa-instagram"></i></a></li>
+					</ul>
+					COPYRIGHT &copy; 2018-2019, INTERNATIONAL MODEL UNITED NATIONS
+				</div>
+			</div>
+		</div>
+	</section>
+<!-- JavaScripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.4/js/tether.min.js"></script>
+</body>
+</html>
